77991c0916fe89e5ccb2aaf28514e5729169171a
prajwal <prajwalthete98@gmail.com>
2024-04-25 10:54:35 +0530

    [Prajwal] Refactor: Improved code indentation for better readability before merging into main

--- BooksStore/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BooksStore/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91717b17cb8b478548474a2044f7010721c8d23c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96063e3f7ee27eb6f3e075da1932ec94e816676f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BooksStore/RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
+++ BooksStore/RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91717b17cb8b478548474a2044f7010721c8d23c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96063e3f7ee27eb6f3e075da1932ec94e816676f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
